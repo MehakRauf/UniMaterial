f1b2f1327f0a73df753acab898219d135fde9e94
Mehak Fatima <139680303+MehakRauf@users.noreply.github.com>
2024-10-17 19:59:00 +0500

    Updated hierarchical modelling lab

--- CG Labs/CarAnimationHierarchical/main.cpp
+++ CG Labs/CarAnimationHierarchical/main.cpp
@@ -2,137 +2,157 @@
 #include <cmath>
 
 int width = 640, height = 480;
-float frameNumber = 0;
+float frameNumber = 0; // For animation, increases by 1 in each frame.
 bool animationRunning = false;
 
 const float PI = 3.14159;
 const int numVertices = 100;
 
-// Function to draw a filled circle (used for tyres)
-void drawCircle(float radius, float r, float g, float b)
+// Draw a circle (used for car tyres)
+void drawCircle(float centerX, float centerY, float radius, float red = 0, float green = 0, float blue = 0)
 {
-    glColor3f(r, g, b);
     glBegin(GL_POLYGON);
+    glColor3f(red, green, blue);
     for (int i = 0; i < numVertices; ++i)
     {
-        float angle = 2.0f * PI * i / numVertices;
-        float x = radius * cos(angle);
-        float y = radius * sin(angle);
+        float angle = 2.0 * PI * static_cast<float>(i) / numVertices;
+        float x = centerX + radius * cos(angle);
+        float y = centerY + radius * sin(angle);
         glVertex2f(x, y);
     }
     glEnd();
 }
 
-// Function to draw the tyre with white circles inside
-void drawTyre(float radius)
+void drawTyre(float centerX, float centerY, float radius)
 {
-    drawCircle(radius, 0, 0, 0); // Black tyre
+    drawCircle(centerX, centerY, radius, 0, 0, 0);
 
-    // Four white circles inside the tyre
-    float innerRadius = radius * 0.3;
-    float offset = radius * 0.6;
-
-    // Left, right, top, bottom circles
-    glPushMatrix();
-    glTranslatef(-offset, 0, 0);
-    drawCircle(innerRadius, 1, 1, 1);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(offset, 0, 0);
-    drawCircle(innerRadius, 1, 1, 1);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0, -offset, 0);
-    drawCircle(innerRadius, 1, 1, 1);
-    glPopMatrix();
-
-    glPushMatrix();
-    glTranslatef(0, offset, 0);
-    drawCircle(innerRadius, 1, 1, 1);
-    glPopMatrix();
+    float innerRadius = radius * 0.2;
+    float offset = radius * 0.5;
+    drawCircle(centerX - offset, centerY, innerRadius, 1, 1, 1);
+    drawCircle(centerX + offset, centerY, innerRadius, 1, 1, 1);
+    drawCircle(centerX, centerY - offset, innerRadius, 1, 1, 1);
+    drawCircle(centerX, centerY + offset, innerRadius, 1, 1, 1);
 }
 
-// Function to draw an arc (used for the rounded sides of the car)
-void drawArc(float radius, float startAngle, float sweepAngle)
+
+void drawArc(float centerX, float centerY, float radius, float startAngle, float sweep)
 {
-    int numSegments = 50;
-    float angleIncrement = sweepAngle * PI / (180.0f * numSegments);
+    const int n = 30;                      // Number of segments in the arc
+    float angle = startAngle * PI / 180.0; // Convert to radians
+    float angleInc = sweep * PI / (180.0 * n);
 
     glBegin(GL_POLYGON);
-    for (int i = 0; i <= numSegments; ++i)
+    for (int i = 0; i <= n; ++i)
     {
-        float angle = (startAngle + i * (sweepAngle / numSegments)) * PI / 180.0f;
-        float x = radius * cos(angle);
-        float y = radius * sin(angle);
+        float x = centerX + radius * cos(angle);
+        float y = centerY + radius * sin(angle);
         glVertex2f(x, y);
+        angle += angleInc;
     }
     glEnd();
 }
 
-// Function to draw the car body (rectangle with arcs on the sides)
+// Draw the car body
 void drawBody()
 {
-    glColor3f(1, 0, 0); // Red color for the body
+    glColor3f(1, 0, 0);
 
-    // Middle rectangle of the car
+    // Middle rectangle for the car body
     glBegin(GL_POLYGON);
-    glVertex2f(0, 0);    // Bottom-left
-    glVertex2f(120, 0);  // Bottom-right
-    glVertex2f(120, 60); // Top-right
-    glVertex2f(0, 60);   // Top-left
+    glVertex2f(228, 130);
+    glVertex2f(320, 130);
+    glVertex2f(320, 212);
+    glVertex2f(228, 212);
     glEnd();
 
-    // Left arc (front of the car)
-    glPushMatrix();
-    glTranslatef(0, 30, 0); // Position the arc
-    drawArc(30, 90, 180);   // Draw a half-circle arc
-    glPopMatrix();
+    // Left arc
+    drawArc(205, 170, 40.0, 90.0, 180.0);
 
-    // Right arc (back of the car)
-    glPushMatrix();
-    glTranslatef(120, 30, 0); // Position the arc
-    drawArc(30, 270, 180);    // Draw a half-circle arc
-    glPopMatrix();
+    // Right arc
+    drawArc(343, 170, 40.0, 270.0, 180.0);
+
+    glBegin(GL_POLYGON);
+    glVertex2f(200, 130);
+    glVertex2f(228, 130);
+    glVertex2f(228, 212);
+    glVertex2f(200, 212);
+    glEnd();
+
+    glBegin(GL_POLYGON);
+    glVertex2f(320, 130);
+    glVertex2f(348, 130);
+    glVertex2f(348, 212);
+    glVertex2f(320, 212);
+    glEnd();
 }
 
-// Function to draw the car roof
+// Draw the car roof
 void drawRoof()
 {
-    glColor3f(0, 0, 0); // Black color for the roof
-
+    glColor3f(0, 0, 0);
     glBegin(GL_POLYGON);
-    glVertex2f(10, 60);  // Bottom-left of roof
-    glVertex2f(110, 60); // Bottom-right of roof
-    glVertex2f(90, 90);  // Top-right
-    glVertex2f(30, 90);  // Top-left
+    glVertex2f(210, 212);
+    glVertex2f(338, 212);
+    glVertex2f(318, 250);
+    glVertex2f(230, 250);
     glEnd();
 }
 
-// Function to draw the door handles
+// Draw the door handles
 void drawHandles()
 {
-    glColor3f(0.5, 0.5, 0.5); // Grey handles
+    glColor3f(0.5, 0.5, 0.5);
 
     // Left door handle
     glBegin(GL_POLYGON);
-    glVertex2f(25, 35); // Top-left
-    glVertex2f(35, 35); // Top-right
-    glVertex2f(35, 30); // Bottom-right
-    glVertex2f(25, 30); // Bottom-left
+    glVertex2f(245, 170);
+    glVertex2f(255, 170);
+    glVertex2f(255, 160);
+    glVertex2f(245, 160);
     glEnd();
 
     // Right door handle
     glBegin(GL_POLYGON);
-    glVertex2f(75, 35); // Top-left
-    glVertex2f(85, 35); // Top-right
-    glVertex2f(85, 30); // Bottom-right
-    glVertex2f(75, 30); // Bottom-left
+    glVertex2f(295, 170);
+    glVertex2f(305, 170);
+    glVertex2f(305, 160);
+    glVertex2f(295, 160);
     glEnd();
 }
 
-// Function to draw the car
+// Draw the road
+void drawRoad()
+{
+    glColor3f(0.5, 0.5, 0.5); // Grey road
+    glBegin(GL_POLYGON);
+    glVertex2f(0, 0);
+    glVertex2f(width, 0);
+    glVertex2f(width, 150);
+    glVertex2f(0, 150);
+    glEnd();
+
+    glLineWidth(10);
+    glColor3f(1, 1, 1); // Stripes on the road
+    glBegin(GL_LINES);
+    glVertex2f(0, 55);
+    glVertex2f(width, 55);
+    glEnd();
+}
+
+// Draw the sky
+void drawSky()
+{
+    glColor3f(0.6, 0.8, 1.0); // Light blue color for the sky
+    glBegin(GL_QUADS);
+    glVertex2f(0, 120);
+    glVertex2f(width, 120);
+    glVertex2f(width, height);
+    glVertex2f(0, height);
+    glEnd();
+}
+
+// Draw the car
 void drawCar()
 {
     glPushMatrix();
@@ -147,54 +167,22 @@ void drawCar()
     glPushMatrix();
     // Draw door handles
     drawHandles();
-    // // Draw the left tyre
+    // Draw the left tyre
     glTranslatef(-1.5, -1, 0);
     glPushMatrix();
 
-    drawTyre(20);
+    drawTyre(200.0, 130.0, 30.0);
     glPopMatrix();
 
     // Draw the right tyre
-    glTranslatef(110, 0, 0);
+    glTranslatef(4, -1, 0);
     glPushMatrix();
-    drawTyre(20);
+    drawTyre(350.0, 130.0, 30.0);
     glPopMatrix();
 
-    glPopMatrix(); // Restore the transformation stack
+    glPopMatrix(); 
     glPopMatrix();
     glPopMatrix();
-}
-
-// Function to draw the road
-void drawRoad()
-{
-    glColor3f(0.3, 0.3, 0.3); // Grey road
-    glBegin(GL_POLYGON);
-    glVertex2f(0, 0);
-    glVertex2f(width, 0);
-    glVertex2f(width, 60);
-    glVertex2f(0, 60);
-    glEnd();
-
-    // White dashed line
-    glColor3f(1, 1, 1);
-    glLineWidth(5);
-    glBegin(GL_LINES);
-    glVertex2f(0, 30);
-    glVertex2f(width, 30);
-    glEnd();
-}
-
-// Function to draw the sky
-void drawSky()
-{
-    glColor3f(0.6, 0.8, 1.0); // Light blue sky
-    glBegin(GL_QUADS);
-    glVertex2f(0, 60);
-    glVertex2f(width, 60);
-    glVertex2f(width, height);
-    glVertex2f(0, height);
-    glEnd();
 }
 
 // Timer function for animation
@@ -202,32 +190,31 @@ void timer(int)
 {
     frameNumber += 1;
     glutPostRedisplay();
-    glutTimerFunc(30, timer, 0);
+    glutTimerFunc(30, timer, 1);
 }
 
-// Menu handling function
+// Menu
 void menu(int option)
 {
     switch (option)
     {
-    case 1: // Start animation
+    case 1: // Start
         if (!animationRunning)
         {
-            glutTimerFunc(0, timer, 0);
+            glutTimerFunc(0, timer, 1);
             animationRunning = true;
         }
         break;
-    case 2: // Restart animation
+    case 2: // Restart
         frameNumber = 0;
         break;
-    case 3: // Exit the program
+    case 3: // Exit
         exit(0);
         break;
     }
     glutPostRedisplay();
 }
 
-// Display function
 void display()
 {
     glClear(GL_COLOR_BUFFER_BIT);
@@ -235,16 +222,14 @@ void display()
     drawSky();
     drawRoad();
 
-    // Animate the car along the road
     glPushMatrix();
-    glTranslatef(-(640 - frameNumber * 6), 70, 0); // Move the car
+    glTranslatef(-(640 - frameNumber * 6), 15, 0); 
     drawCar();
     glPopMatrix();
 
     glFlush();
 }
 
-// Initialization function
 void init()
 {
     glMatrixMode(GL_MODELVIEW);
@@ -252,25 +237,23 @@ void init()
     gluOrtho2D(0, width, 0, height);
 }
 
-// Main function
 int main(int argc, char **argv)
 {
     glutInit(&argc, argv);
     glutInitDisplayMode(GLUT_SINGLE | GLUT_RGB);
     glutInitWindowSize(width, height);
-    glutCreateWindow("2D Hierarchical Car Model");
-
+    glutInitWindowPosition(50, 50);
+    glutCreateWindow("Hierarchical Car Modelling");
     init();
 
-    // Create the menu
     glutCreateMenu(menu);
     glutAddMenuEntry("Start", 1);
     glutAddMenuEntry("Restart", 2);
     glutAddMenuEntry("Exit", 3);
-    glutAttachMenu(GLUT_RIGHT_BUTTON); // Right-click for menu
+    glutAttachMenu(GLUT_RIGHT_BUTTON);
 
+    glShadeModel(GL_FLAT);
     glutDisplayFunc(display);
-
     glutMainLoop();
     return 0;
 }
